5a5b5adefa7af36cf1b903dbf19413059fc64934
DM <podanevd@phoenixit.ru>
2023-06-12 23:27:37 +0300

    init package.json & webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const FileManagerPlugin = require("filemanager-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: ["@babel/polyfill", "./src/index.jsx"],
+    resolve: {
+        extensions: ['.js','.jsx', '.json', '.wasm'],
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: "[name].[contenthash].js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/, // обновляем регулярное выражение для поддержки jsx
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true,
+                        presets: ["@babel/preset-react", "@babel/preset-env"]
+                    },
+                },
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        }),
+        new FileManagerPlugin({
+            events: {
+                onStart: {
+                    delete: ['dist'],
+                },
+            },
+        }),
+    ]
+}
